8cac97f9d01696d669e3220f106dc984dc918527
choi.techwin <choi.techwin@gmail.com>
2023-01-12 23:02:21 +0900

    1

--- 03Technical/MemoryManager/Memory.h
+++ 03Technical/MemoryManager/Memory.h
@@ -4,7 +4,59 @@
 #include "../../01Base/Object/BaseObject.h"
 #include "../../01Base/Memory/IMemory.h"
 
-//#include "../../2Platform/Component/ComponentPart.h"
+#define PAGESIZE 1024
+class Page {
+public:
+	Page* pNext;
+};
+
+class PageList {
+private:
+	char* pMemoryAllocated;
+	size_t numPagesTotal;
+
+	Page* pHeadFree;
+	size_t numPagesFree;
+
+	Page* pHeadAllocated;
+
+	size_t GetIndex(Page* pPage) {
+		size_t index = (reinterpret_cast<size_t>(pPage) - reinterpret_cast<size_t>(this->pMemoryAllocated)) / PAGESIZE;
+	}
+
+public:
+	PageList(size_t szAllocated, char* pMemoryAllocated) {
+		this->pMemoryAllocated = pMemoryAllocated;
+		this->numPagesTotal = szAllocated / PAGESIZE;
+		if (numPagesTotal < 1) {
+			throw Exception(static_cast<int>(IMemory::EException::_eOutOfMemory));
+		}
+
+		this->pHeadFree = reinterpret_cast<Page*>(this->pMemoryAllocated);
+		Page* pPage;
+		for (pPage = this->pHeadFree; pPage + PAGESIZE <= this->pHeadFree + szAllocated; pPage += PAGESIZE) {
+			pPage->pNext = pPage + PAGESIZE;
+		}
+		pPage->pNext = nullptr;
+
+		this->pHeadAllocated = nullptr;
+		this->numPagesFree = this->numPagesTotal;
+	}
+
+	Page* Allocate() {
+		if (this->pHeadFree == nullptr) {
+			throw Exception(static_cast<int>(IMemory::EException::_eOutOfMemory));
+		}
+		Page* pNewPage = this->pHeadFree;
+		this->pHeadFree = this->pHeadFree->pNext;
+		this->numPagesFree++;
+	}
+	void Delocate(Page* pPage) {
+		pPage->pNext = this->pHeadFree;
+		this->pHeadFree = pPage;
+		this->numPagesFree--;
+	}
+};
 
 class Memory :public IMemory, public BaseObject
 {
@@ -26,62 +78,12 @@ public:
 	}
 
 private:
-#define PAGESIZE 1024
-	class Page {
-	public:
-		Page* pNext;
-	};
-
-	class PageList {
-	private:
-		Page** freePageList;
-		Page** allocatedPageList;
-	public:
-		PageList(size_t szAllocated, char* pMemoryAllocated) {
-			if (szAllocated < PAGESIZE) {
-				throw Exception(static_cast<int>(Memory::EException::_eOutOfMemory));
-			}
-			this->freePageList = reinterpret_cast<Page**>(pMemoryAllocated);
-
-			this->pHead = reinterpret_cast<Page*>(pMemoryAllocated);
-			Page* pPage;
-			for (pPage = this->pHead; pPage + PAGESIZE <= this->pHead + szAllocated; pPage += PAGESIZE) {
-				pPage->pNext = pPage + PAGESIZE;
-			}
-			pPage->pNext = nullptr;
-		}
-
-		Page* Allocate() {
-			if (this->pHead == nullptr) {
-				throw Exception(static_cast<int>(Memory::EException::_eOutOfMemory));
-			}
-			Page* pNewPage = this->pHead;
-			this->pHead = this->pHead->pNext;
-			this->pInUse
-		}
-		Page* Delocate() {
-
-		}
-	};
-
 	// attributes
 	size_t m_szAllocated;
 	void* m_pAllocated;
 
 	virtual void* Malloc(size_t szAllocate, const char* pcName = "") = 0;
 	virtual void Free(void* ptr) = 0;
-
-public:
-	// critical section
-	virtual void Lock() = 0;
-	virtual void UnLock() = 0;
-
-	size_t GetSzAllocated() { return this->m_szAllocated; }
-	void SetSzAllocated(size_t szAllocated) { this->m_szAllocated = szAllocated; }
-
-	void* GetPAllocated() { return this->m_pAllocated; }
-	void SetPAllocated(void* pAllocated) { this->m_pAllocated = pAllocated; }
-
 public:
 	// constructors and destructors
 	Memory(int nClassId = _Memory_Id,
@@ -98,6 +100,16 @@ public:
 	}
 	virtual void Finalize() {
 	}	
+
+	// critical section
+	virtual void Lock() = 0;
+	virtual void UnLock() = 0;
+
+	size_t GetSzAllocated() { return this->m_szAllocated; }
+	void SetSzAllocated(size_t szAllocated) { this->m_szAllocated = szAllocated; }
+
+	void* GetPAllocated() { return this->m_pAllocated; }
+	void SetPAllocated(void* pAllocated) { this->m_pAllocated = pAllocated; }
 
 	// methods
 	void* SafeMalloc(size_t szAllocate, const char *pcName)
