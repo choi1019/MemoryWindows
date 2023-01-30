3d5eb891c0d512ab2cb0d8c7a50711e49f00efeb
choi1019 <choi.sungwoon@gmail.com>
2023-01-30 21:14:58 +0900

    1

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -43,5 +43,4 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/PLatformLinux.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/PLatformLinux.dir/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/PLatformLinux.dir/main.cpp.o: \
+ /home/choisw1019/ProjectsCPP/PlatformLinux/main.cpp \
+ /usr/include/stdc-predef.h
